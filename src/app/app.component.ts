33630bb33e9ccdec2b3b070356ec3a024490796f
yunomi811 <aldova811@gmail.com>
2023-07-22 11:14:06 +0700

    Template first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'aldova-movies-app';
+  title = 'showtime';
+  navbg: any = {
+    'background-color': '#ffffff33',
+    'box-shadow': 'rgba(0, 0, 0, 0.24) 0px 3px 8px',
+  }
+
+  @HostListener('document:scroll') scrollover() {
+    console.log(document.body.scrollTop, 'scrolllength#');
+
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+      this.navbg = {
+        'background-color': '#33333333',
+        'box-shadow': 'rgba(255, 255, 255, 0.24) 0px 3px 8px',
+      }
+    } else {
+      this.navbg = {
+        'background-color': '#ffffff33',
+        'box-shadow': 'rgba(0, 0, 0, 0.24) 0px 3px 8px'
+      }
+    }
+  }
 }
